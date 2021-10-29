f288595d97d814071fd8d13a06551ab351bcbe8a
alistair-ward <alistair@fred.gen.nz>
2021-10-29 19:52:02 +1300

    ref:curve: #59 WIP Simple curve calculations
    Change to CamelCase.
    Implement tests for simple curve case.
    Implement curve calculations for a simple (single radius) curve.
    Implement calculate-on-demand in TCurve class.

--- model/curve.pas
+++ model/curve.pas
@@ -24,37 +24,124 @@ const
   max_rad_test: double = maximum_radius_value - 10000;
 
 
-
 type
-  Tcurve = class
+  TCurve = class
   private
-    Fnominal_radius: double;
-    Fis_spiral: boolean;
+    FModified: boolean;
+    FNominalRadius: double;
+    FIsSpiral: boolean;
+    FIsStraight: boolean;
+    FIsSimpleCurve: boolean;
+
+    FOrigin1: Tpex;
+
+    procedure CheckModified;
+    procedure CalculateCurveConstants;
+    procedure SetNominalRadius(const newRadius: double);
+    procedure SetIsSpiral(const newIsSpiral: boolean);
+    function GetIsStraight: boolean;
+    function GetIsSimpleCurve: boolean;
 
   public
-    property nominal_radius: double read Fnominal_radius write Fnominal_radius;
-    property isSpiral: boolean read Fis_spiral write Fis_spiral;
+    constructor Create;
 
-    procedure do_calculation(distance, offset: double; out pt, direction: Tpex; out radius: double);
+    property nominalRadius: double Read FNominalRadius Write SetNominalRadius;
+    property isSpiral: boolean Read FIsSpiral Write SetIsSpiral;
+    property isStraight: boolean Read GetIsStraight;
+    property isSimpleCurve: boolean Read GetIsSimpleCurve;
+
+    procedure DoCalculation(distance, offset: double; out pt, direction: Tpex; out radius: double);
   end;
 
 implementation
 
-procedure Tcurve.do_calculation(distance, offset: double; out pt, direction: Tpex; out radius: double);
+constructor TCurve.Create;
 begin
- if (ABS(Fnominal_radius) > max_rad_test) and (not Fis_spiral) then
- begin
-   // Straight line
-   pt.set_xy(distance, offset);
-   direction.set_xy(1, 0);
-   radius := max_rad;
-   Exit;
- end;
+  // default is straight line
+  FModified := True;
+  FNominalRadius := max_rad;
+  FIsSpiral := False;
+end;
 
- pt.set_xy(0,0);
- direction.set_xy(1,0);
- radius := max_rad;
+procedure TCurve.SetNominalRadius(const newRadius: double);
+begin
+  if (newRadius <> FNominalRadius) then begin
+    FModified := True;
+    FNominalRadius := newRadius;
+  end;
+end;
+
+procedure TCurve.SetIsSpiral(const newIsSpiral: boolean);
+begin
+  if (newIsSpiral <> FIsSpiral) then begin
+    FModified := True;
+    FIsSpiral := newIsSpiral;
+  end;
+end;
+
+procedure TCurve.CheckModified;
+begin
+  if FModified then begin
+    CalculateCurveConstants;
+    FModified := False;
+  end;
+end;
+
+procedure TCurve.CalculateCurveConstants;
+begin
+  FIsStraight := (Abs(FNominalRadius) > max_rad_test) and not FIsSpiral;
+  FIsSimpleCurve := not FIsStraight and not FIsSpiral;
+
+  if FIsStraight then begin
+    // nothing special to calculate for a straight line
+  end
+  else
+  if FIsSimpleCurve then begin
+    // calculate origin of circle
+    FOrigin1.set_xy(0, FNominalRadius);
+  end
+  else begin
+  end;
+end;
+
+procedure TCurve.DoCalculation(distance, offset: double; out pt, direction: Tpex;
+  out radius: double);
+var
+  turnAngle: double;
+  actualRadius: double;
+begin
+  CheckModified;
+
+  if FIsStraight then begin
+    // Straight line
+    pt.set_xy(distance, offset);
+    direction.set_xy(1, 0);
+    radius := max_rad;
+    Exit;
+  end;
+
+  // simple curve
+  turnAngle := distance / FNominalRadius;
+  direction.set_xy(cos(turnAngle), sin(turnAngle));
+
+  actualRadius := FNominalRadius - offset;
+
+  pt.set_xy( FOrigin1.x + actualRadius * direction.y, FOrigin1.y - actualRadius * direction.x);
+  radius := FNominalRadius;
+end;
+
+function TCurve.GetIsStraight: boolean;
+begin
+  CheckModified;
+  Result := FIsStraight;
+end;
+
+function TCurve.GetIsSimpleCurve: boolean;
+begin
+  CheckModified;
+  Result := FIsSimpleCurve;
 end;
 
 end.
+
 
