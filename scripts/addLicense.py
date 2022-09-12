057e861c443544349111bfbeb31ffcc5458960a9
Graeme Defty <graeme.defty@gmail.com>
2022-09-12 16:11:07 +0700

    ref:misc: Move scripts to a 'scripts' directory + minor documentation changes

--- scripts/addLicense.py
+++ scripts/addLicense.py
@@ -20,6 +20,12 @@ licenseText = """
 
     Or if not, refer to the web site: https://www.gnu.org/licenses/
 
+                >>>     NOTE TO DEVELOPERS     <<<
+                     DO NOT EDIT THIS COMMENT
+              It is inserted in this file by running
+                  'python3 scripts/addComment.py'
+         The original text lives in scripts/addComment.py.
+
 ====================================================================================
 *)
 
@@ -78,7 +84,7 @@ def do_file(path_object):
         file.writelines(lines[ix:])
 
 
-    
+
 # --------------------------------------------------------------#
 
 root_directory = Path(".")
